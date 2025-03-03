c6fe11e8cd6532644a872557b1e5fc43b0be727b
Aubrey San Jose <mary.sanjose@student.ateneo.edu>
2025-03-03 16:55:00 +0800

    Finished creating class for preprocessing. Although idk if applicable to convert Tagalog sentences also. Anyway, at least we're done with step 1. Albert, do the Step 2 cuz idk what's next.

--- translator.py
+++ translator.py
@@ -1,14 +1,32 @@
 import pandas as pd
 import torch
-import numpy as nn
+import numpy as np
+import torch.nn as nn
+from collections import Counter
 
-class EngFilTranslationModel(nn.Module):
+class EngTagTranslationModel(nn.Module):
     def __init__(self, vocab_size, embedding_dim):
-        super(EngFilTranslationModel, self).__init__()
+        super(EngTagTranslationModel, self).__init__()
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
         self.linear_layer_1 = nn.Linear(embedding_dim, 5)
         self.output_layer = nn.Linear(5, 5)
 
+    def build_vocab(texts):
+        word_counter = Counter()
+        for text in texts:
+            text = text.replace('!', ' <exclaim>')
+            text = text.replace('.', ' <period>')
+            text = text.replace('?', ' <question>')
+            text = text.lower()
+            words = text.split()
+            word_counter.update(words)
+
+        vocab = {word: idx+1 for idx, (word, _) in enumerate(word_counter.most_common())}
+        vocab["<unk>"] = 0  # Assign index 0 to unknown words
+        return vocab
  
 df = pd.read_csv("english_to_tagalog_1000.csv")
-print(df)
+en_vocab = EngTagTranslationModel.build_vocab(df["English"].tolist())
+print(en_vocab)
+tag_vocab = EngTagTranslationModel.build_vocab(df["Tagalog"].tolist())
+print(tag_vocab)
